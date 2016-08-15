d9b80e79667186983a10f1b3eec13a116d868aaa
Miralas <miralas777@yandex.ru>
2016-08-15 14:34:00 +0300

    Add Lightbox

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,5 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require agency
+//= require lightbox-bootstrap
+//= require lightbox_bootstraped
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,4 @@ Rails.application.config.assets.precompile += %w( admin.js )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( agency.js )
+# Rails.application.config.assets.precompile += %w( lightbox/* )
